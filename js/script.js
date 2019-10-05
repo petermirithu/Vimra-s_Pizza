67d3f5f7d8971b2aaf119af90ce5234d78bb10e6
petermirithu <pyra_m.k@yahoo.com>
2019-10-05 13:45:16 +0300

    add an if statement for storing data on size and crust of the pizza.

--- js/script.js
+++ js/script.js
@@ -25,9 +25,11 @@ function Deliver(county, place) {
   this.place = place;
 }
 
+var counter= 1;
+
 $(document).ready(function() {
   $("#another-pizza").click(function() {
-    $("#new-orders").append('<div class="form-group">'+
+    var $newDiv = $('<div class="form-group">'+
     '<label for="pizza-size">Pizza Size:</label>'+
     '<input list="p-size" type="p-size" id="pizza-size" class="form-control">'+
     '<datalist id="p-size">'+
@@ -73,6 +75,9 @@ $(document).ready(function() {
       '</datalist>'+
     '</div>'+
   '</div>');
+  $newDiv.attr("id", "newDiv" + counter++);
+  $("#new-orders").append($newDiv);
+
   });
   $("#add-topping").click(function() {
     $(".add-top").append('<div class="form-group">'+
@@ -145,20 +150,200 @@ $(document).ready(function() {
     
     var inputSize = $("input#pizza-size").val();
     if (inputSize == "Small") {
-      var smallP = 100;
+      var size0 = 100;
     }else if (inputSize=="Medium") {
-      var mediumP = 200;
+      var size0 = 200;
     }else if (inputSize=="Large") {
-      var largep= 300
+      var size0= 300
     }else{
-      alert("Please pick size from the menu only")
+      var size0=0;
     }
+    
+    var inputSize =$("#newDiv1 input#pizza-size").val();
+    if (inputSize == "Small") {
+      var size1 = 100;
+    }else if (inputSize=="Medium") {
+      var size1 = 200;
+    }else if (inputSize=="Large") {
+      var size1= 300
+    }else{
+      var size1=0;
+    }
+
+    var inputSize =$("#newDiv2 input#pizza-size").val();
+    if (inputSize == "Small") {
+      var size2 = 100;
+    }else if (inputSize=="Medium") {
+      var size2 = 200;
+    }else if (inputSize=="Large") {
+      var size2= 300
+    }else{
+      var size2=0;
+    }
+
+    var inputSize =$("#newDiv3 input#pizza-size").val();
+    if (inputSize == "Small") {
+      var size3 = 100;
+    }else if (inputSize=="Medium") {
+      var size3 = 200;
+    }else if (inputSize=="Large") {
+      var size3= 300
+    }else{
+      var size3=0;
+    }
+
+    var inputSize =$("#newDiv4 input#pizza-size").val();
+    if (inputSize == "Small") {
+      var size4 = 100;
+    }else if (inputSize=="Medium") {
+      var size4 = 200;
+    }else if (inputSize=="Large") {
+      var size4= 300
+    }else{
+      var size4=0;
+    }
+
     var inputCrust = $("input#pizza-crust").val();
+    if (inputCrust=="Cripsy crust") {
+      var crust0=400;
+    }else if (inputCrust=="Craker Crust"){
+      var crust0=420;
+    }else if (inputCrust=="Pizza Bagels"){
+      var crust0=450;
+    }else if (inputCrust=="Detroit Crust"){
+      var crust0=500;
+    }else if (inputCrust=="Sicilian Style"){
+      var crust0=550;
+    }else if (inputCrust=="Flat-bread Crust"){
+      var crust0=580;
+    }else if (inputCrust=="Chicago Deep Dish"){
+      var crust0=650;  
+    }else if (inputCrust=="Pretzel Crust"){
+      var crust0=700;
+    }else if (inputCrust=="NYC-Style Crust"){
+      var crust0=750;
+    }else if (inputCrust=="Stuffed Dough"){
+      var crust0=750;
+    }else if (inputCrust=="Focaccia"){
+      var crust0=800;
+    }else{
+      var crust0=0;
+    }
+
+    var inputCrust =$("#newDiv1 input#pizza-crust").val();
+    if (inputCrust=="Cripsy crust") {
+      var crust1=400;
+    }else if (inputCrust=="Craker Crust"){
+      var crust1=420;
+    }else if (inputCrust=="Pizza Bagels"){
+      var crust1=450;
+    }else if (inputCrust=="Detroit Crust"){
+      var crust1=500;
+    }else if (inputCrust=="Sicilian Style"){
+      var crust1=550;
+    }else if (inputCrust=="Flat-bread Crust"){
+      var crust1=580;
+    }else if (inputCrust=="Chicago Deep Dish"){
+      var crust1=650;  
+    }else if (inputCrust=="Pretzel Crust"){
+      var crust1=700;
+    }else if (inputCrust=="NYC-Style Crust"){
+      var crust1=750;
+    }else if (inputCrust=="Stuffed Dough"){
+      var crust1=750;
+    }else if (inputCrust=="Focaccia"){
+      var crust1=800;
+    }else{
+      var crust1=0;
+    }
+
+    var inputCrust =$("#newDiv2 input#pizza-crust").val();
+    if (inputCrust=="Cripsy crust") {
+      var crust2=400;
+    }else if (inputCrust=="Craker Crust"){
+      var crust2=420;
+    }else if (inputCrust=="Pizza Bagels"){
+      var crust2=450;
+    }else if (inputCrust=="Detroit Crust"){
+      var crust2=500;
+    }else if (inputCrust=="Sicilian Style"){
+      var crust2=550;
+    }else if (inputCrust=="Flat-bread Crust"){
+      var crust2=580;
+    }else if (inputCrust=="Chicago Deep Dish"){
+      var crust2=650;  
+    }else if (inputCrust=="Pretzel Crust"){
+      var crust2=700;
+    }else if (inputCrust=="NYC-Style Crust"){
+      var crust2=750;
+    }else if (inputCrust=="Stuffed Dough"){
+      var crust2=750;
+    }else if (inputCrust=="Focaccia"){
+      var crust2=800;
+    }else{
+      var crust2=0;
+    }
+    
+    var inputCrust =$("#newDiv3 input#pizza-crust").val();
+    if (inputCrust=="Cripsy crust") {
+      var crust3=400;
+    }else if (inputCrust=="Craker Crust"){
+      var crust3=420;
+    }else if (inputCrust=="Pizza Bagels"){
+      var crust3=450;
+    }else if (inputCrust=="Detroit Crust"){
+      var crust3=500;
+    }else if (inputCrust=="Sicilian Style"){
+      var crust3=550;
+    }else if (inputCrust=="Flat-bread Crust"){
+      var crust3=580;
+    }else if (inputCrust=="Chicago Deep Dish"){
+      var crust3=650;  
+    }else if (inputCrust=="Pretzel Crust"){
+      var crust3=700;
+    }else if (inputCrust=="NYC-Style Crust"){
+      var crust3=750;
+    }else if (inputCrust=="Stuffed Dough"){
+      var crust3=750;
+    }else if (inputCrust=="Focaccia"){
+      var crust3=800;
+    }else{
+      var crust3=0;
+    }
+
+    var inputCrust =$("#newDiv4 input#pizza-crust").val();
+    if (inputCrust=="Cripsy crust") {
+      var crust4=400;
+    }else if (inputCrust=="Craker Crust"){
+      var crust4=420;
+    }else if (inputCrust=="Pizza Bagels"){
+      var crust4=450;
+    }else if (inputCrust=="Detroit Crust"){
+      var crust4=500;
+    }else if (inputCrust=="Sicilian Style"){
+      var crust4=550;
+    }else if (inputCrust=="Flat-bread Crust"){
+      var crust4=580;
+    }else if (inputCrust=="Chicago Deep Dish"){
+      var crust4=650;  
+    }else if (inputCrust=="Pretzel Crust"){
+      var crust4=700;
+    }else if (inputCrust=="NYC-Style Crust"){
+      var crust4=750;
+    }else if (inputCrust=="Stuffed Dough"){
+      var crust4=750;
+    }else if (inputCrust=="Focaccia"){
+      var crust4=800;
+    }else{
+      var crust4=0;
+    }
+    alert(crust0+crust1+crust2+crust3+crust4);
+
     var inputTop = $("input#pizza-top").val();
 
     var placeNairobi = $("input#place-nairobi").val();
     var placeNakuru = $("input#place-nakuru").val();
     var placeMombasa = $("input#place-mombasa").val();
-    
+  
   });
 });
